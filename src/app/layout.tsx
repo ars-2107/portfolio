3c859c9f51cb4f7ce5127d2cfbd4c230a4faea21
Apoorv Sharma <apoorvsharma2107@gmail.com>
2025-04-15 07:36:06 +0530

    ✨ update head title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'ARS - Portfolio',
+  title: 'Portfolio | ARS',
   description: 'I&apos;m a detail-oriented Full Stack Developer with a creative approach and a strong problem-solving ability. My expertise spans a wide range of technologies, allowing me to build innovative solutions across web applications, mobile applications, and the fields of data science and machine learning. I enjoy tackling challenges head-on and turning ideas into practical, user-friendly solutions. Committed to continuous learning and collaboration, I bring a results-oriented approach to every project.',
 }
 
