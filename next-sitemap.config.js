bf19ba40b3b8b5ade446209b0c3d35c55db41145
Apoorv Sharma <apoorvsharma2107@gmail.com>
2025-04-23 16:25:10 +0530

    🔍️ Add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://apoorvsharma.space/',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: 'daily',
+  priority: 0.7,
+};
