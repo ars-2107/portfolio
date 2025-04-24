71acc9a39ce3bef9f3fed5a309ee04d9575ce26d
Apoorv Sharma <apoorvsharma2107@gmail.com>
2025-04-25 03:22:15 +0530

    🍱 Update site.webmanifest, sitemap.xml and robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://apoorvsharma.space/',
+  siteUrl: 'https://www.apoorvsharma.space/',
   generateRobotsTxt: true,
   sitemapSize: 7000,
   changefreq: 'daily',
